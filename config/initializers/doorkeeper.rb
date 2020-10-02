fe9b66a78b1739369af99f2269034bbdf6834e26
Ivan Shamatov <status.enable@gmail.com>
2020-10-02 11:17:56 +0300

    remove removed ssl

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -266,7 +266,7 @@ Doorkeeper.configure do
   # #call can be used in order to allow conditional checks (to allow non-SSL
   # redirects to localhost for example).
   #
-  # force_ssl_in_redirect_uri !Rails.env.development?
+  force_ssl_in_redirect_uri !Rails.env.development?
   #
 
   # Specify what redirect URI's you want to block during Application creation.
